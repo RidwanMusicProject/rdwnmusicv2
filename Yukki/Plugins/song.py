383b7481335965cf030b45728f6a1a342d71cdf5
Noissy Robot <95743557+RidwanMusicProject@users.noreply.github.com>
2021-12-21 07:03:40 +0700

    Update song.py

--- Yukki/Plugins/song.py
+++ Yukki/Plugins/song.py
@@ -94,7 +94,7 @@ async def musicdl(_, message: Message):
         m = await message.reply_photo(
             photo=thumb,
             reply_markup=InlineKeyboardMarkup(buttons),    
-            caption=(f"🏷 <b>Name:</b> [{title[:65]}]({url})\n\n💡 [Check music information](https://t.me/{BOT_USERNAME}?start=info_{id})")
+            caption=(f"🏷 <b>Name:</b> [{title[:65]}]({url})\n\n💡 [HALBERT](https://t.me/rdwan_13?start=info_{id})")
         )   
         os.remove(thumb)
     else:
@@ -128,7 +128,7 @@ async def musicdl(_, message: Message):
         buttons = search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, duration4, duration5, user_id, query)
         hmo = await message.reply_photo(
             photo=thumb, 
-            caption=(f"1️⃣ <b>[{title1[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID1})\n └ ⚡ __Powered by HALBERT Music AI__\n\n2️⃣ <b>[{title2[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID2})\n └ ⚡ __Powered by HALBERT Music AI__\n\n3️⃣ <b>[{title3[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID3})\n └ ⚡ __Powered by HALBERT Music AI__\n\n4️⃣ <b>[{title4[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID4})\n └ ⚡ __Powered by HALBERT Music AI__\n\n5️⃣ <b>[{title5[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID5})\n └ ⚡ __Powered by HALBERT Music AI__"),
+            caption=(f"1️⃣ <b>[{title1[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID1})\n └ ⚡ __Powered by Ridwan Music AI__\n\n2️⃣ <b>[{title2[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID2})\n └ ⚡ __Powered by Ridwan Music AI__\n\n3️⃣ <b>[{title3[:25]}...]({link})</b>\n ├ 💡 [More [HALBERT](https://t.me/rdwan_13?start=info_{ID3})\n └ ⚡ __Powered by Ridwan Music AI__\n\n4️⃣ <b>[{title4[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID4})\n └ ⚡ __Powered by Ridwan Music AI__\n\n5️⃣ <b>[{title5[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID5})\n └ ⚡ __Powered by Ridwan Music AI__"),
             reply_markup=InlineKeyboardMarkup(buttons),
         )  
         disable_web_page_preview=True
@@ -173,7 +173,7 @@ async def download_data(_,CallbackQuery):
     m = await CallbackQuery.message.reply_photo(
         photo=thumb,
         reply_markup=InlineKeyboardMarkup(buttons),    
-        caption=(f"🏷 <b>Name:</b> [{title[:65]}]({url})\n\n💡 [Check music information](https://t.me/{BOT_USERNAME}?start=info_{id})")
+        caption=(f"🏷 <b>Name:</b> [{title[:65]}]({url})\n\n💡 [HALBERT](https://t.me/rdwan_13?start=info_{id})")
     )   
     os.remove(thumb)
     await CallbackQuery.message.delete()
@@ -232,7 +232,7 @@ async def chonga(_,CallbackQuery):
         link = "https://www.youtube.com/watch?v={id}"
         buttons = search_markup2(ID6, ID7, ID8, ID9, ID10, duration6, duration7, duration8, duration9, duration10 ,user_id, query)
         await CallbackQuery.edit_message_text(
-            f"6️⃣ <b>[{title6[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID6})\n └ ⚡ __Powered by HALBERT Music AI__\n\n7️⃣ <b>[{title7[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID7})\n └ ⚡ __Powered by HALBERT Music AI__\n\n8️⃣ <b>[{title8[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID8})\n └ ⚡ __Powered by HALBERT Music AI__\n\n9️⃣ <b>[{title9[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID9})\n └ ⚡ __Powered by HALBERT Music AI__\n\n🔟 <b>[{title10[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID10})\n └ ⚡ __Powered by HALBERT Music AI__",    
+            f"6️⃣ <b>[{title6[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID6})\n └ ⚡ __Powered by Ridwan Music AI__\n\n7️⃣ <b>[{title7[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID7})\n └ ⚡ __Powered by Ridwan Music AI__\n\n8️⃣ <b>[{title8[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID8})\n └ ⚡ __Powered by Ridwan Music AI__\n\n9️⃣ <b>[{title9[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID9})\n └ ⚡ __Powered by Ridwan Music AI__\n\n🔟 <b>[{title10[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID10})\n └ ⚡ __Powered by Ridwan Music AI__",    
             reply_markup=InlineKeyboardMarkup(buttons),
         )  
         disable_web_page_preview=True
@@ -241,7 +241,7 @@ async def chonga(_,CallbackQuery):
         link = "https://www.youtube.com/watch?v={id}"
         buttons = search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, duration4, duration5, user_id, query)
         await CallbackQuery.edit_message_text(
-            f"1️⃣ <b>[{title1[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID1})\n └ ⚡ __Powered by HALBERT Music AI__\n\n2️⃣ <b>[{title2[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID2})\n └ ⚡ __Powered by HALBERT Music AI__\n\n3️⃣ <b>[{title3[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID3})\n └ ⚡ __Powered by HALBERT Music AI__\n\n4️⃣ <b>[{title4[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID4})\n └ ⚡ __Powered by HALBERT Music AI__\n\n5️⃣ <b>[{title5[:25]}...]({link})</b>\n ├ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{ID5})\n └ ⚡ __Powered by HALBERT Music AI__",    
+            f"1️⃣ <b>[{title1[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID1})\n └ ⚡ __Powered by Ridwan Music AI__\n\n2️⃣ <b>[{title2[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID2})\n └ ⚡ __Powered by Ridwan Music AI__\n\n3️⃣ <b>[{title3[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID3})\n └ ⚡ __Powered by Ridwan Music AI__\n\n4️⃣ <b>[{title4[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID4})\n └ ⚡ __Powered by Ridwan Music AI__\n\n5️⃣ <b>[{title5[:25]}...]({link})</b>\n ├ 💡 [HALBERT](https://t.me/rdwan_13?start=info_{ID5})\n └ ⚡ __Powered by Ridwan Music AI__",    
             reply_markup=InlineKeyboardMarkup(buttons),
         )  
         disable_web_page_preview=True
